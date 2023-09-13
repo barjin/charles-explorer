2d86023476b2c235ad8d5d9bad5369dad5851009
Jindřich Bär <jindrichbar@gmail.com>
2023-09-13 17:51:50 +0200

    fix: fix the data rotation script [ci skip]

--- scripts/rotate-data.sh
+++ scripts/rotate-data.sh
@@ -10,11 +10,12 @@ fi
 
 cd $SCRIPT_DIR/../
 
-docker compose exec db sh -c "psql -U postgres -c 'DROP database charles_explorer_db'"
+docker compose exec db sh -c "psql -U postgres -c 'DROP database charles_explorer_db WITH (FORCE)'"
 
 docker compose exec solr sh -c "solr delete -c class"
 docker compose exec solr sh -c "solr delete -c publication"
 docker compose exec solr sh -c "solr delete -c person"
 docker compose exec solr sh -c "solr delete -c programme"
 
+docker compose --profile='seeder' rm -f
 docker compose --profile='seeder' up
