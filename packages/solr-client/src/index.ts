ef65575689cec89defd51a1e1750bf102572b434
Jindřich Bär <jindrichbar@gmail.com>
2023-09-28 00:56:53 +0200

    docs: add JSDoc comments for solr-client

--- packages/solr-client/src/index.ts
+++ packages/solr-client/src/index.ts
@@ -1,10 +1,21 @@
 import { getTextFields, db } from '@charles-explorer/prisma';
 import axios from 'axios';
 
+/**
+ * SolrCollection is a wrapper around a Solr core with a simple API for creating the search core, adding documents and performing search.
+ */
 class SolrCollection {
+    /**
+     * The underlying Solr instance.
+     */
     private solr: Solr;
+    /**
+     * The name of the Solr core.
+     */
     private collection: string;
-    private emptyResponse = [];
+    /**
+     * Whether the Solr core exists. This is `false` until the first call to `createIfNotExists()`.
+     */
     private exists = false;
 
     constructor(solr: Solr, collection: string) {
@@ -12,6 +23,12 @@ class SolrCollection {
         this.collection = collection;
     }
 
+    /**
+     * Creates the Solr core if it does not exist yet.
+     * 
+     * First checks if the core exists by calling the Solr API. 
+     * If it does not exist, it creates it using the `sharedConfig` config set containing the `managed-schema.xml` and `solrconfig.xml` files.
+     */
     public async createIfNotExists() {
         if(this.exists) return;
 
@@ -34,10 +51,21 @@ class SolrCollection {
         this.exists = true;
     }
 
+    /**
+     * Returns the URL of the Solr core with the given slug.
+     * @param slug The slug to append to the URL.
+     * @returns The URL of the Solr core with the given slug.
+     */
     private getSelfUrl(slug = '') {
         return new URL(`/solr/${this.collection}/${slug}`, this.solr.url).href;
     }
 
+    /**
+     * Adds a single document to the Solr core. This document has to follow the schema defined in `managed-schema.xml`.
+     * 
+     * In case you are adding multiple documents, use `addDocuments()` instead, which is more efficient.
+     * @param document The document to add.
+     */
     async addDocument(document: any) {
         await this.createIfNotExists();
 
@@ -48,6 +76,10 @@ class SolrCollection {
         });
     }
 
+    /**
+     * Adds multiple documents to the Solr core. These documents have to follow the schema defined in `managed-schema.xml`.
+     * @param documents The array of documents to add.
+     */
     async addDocuments(documents: any[]) {
         await this.createIfNotExists();
 
@@ -58,20 +90,36 @@ class SolrCollection {
         });
     }
 
+    /**
+     * Runs the Solr commit and suggest build commands.
+     * 
+     * This causes the Solr core to update its index and build the suggester. It might take a while to finish.
+     */
     async finish() {
         await axios.get(this.getSelfUrl('update?commit=true'));
         await axios.get(this.getSelfUrl('suggest?suggest.buildAll=true'));
     }
     
+    /**
+     * Escapes special characters in a Solr query.
+     * @param query The query to escape.
+     * @returns The escaped query.
+     */
     private escapeSolrQuery(query: string){
         const specialChars = ["+","-","&","|","!","(",")","{","}","[","]","^","\"","~","*","?",":","/"];
     
         return specialChars.reduce((acc, char) => acc.replaceAll(char, `\\${char}`), query).split(' ').join(' ');
     }
     
+    /**
+     * Performs a search in the Solr core and returns the IDs of the matching documents.
+     * @param query Query to search for.
+     * @param options Options for the search (e.g. number of results to return or IDs to filter by)
+     * @returns Array of IDs of the matching documents with their relevance scores.
+     */
     async searchIds(query: string, options?: { rows?: number, ids?: string[] }): Promise<{ id: string, score: number }[]> {
         if (!query) {
-            return Promise.resolve(this.emptyResponse);
+            return Promise.resolve([]);
         }
     
         const escapedQuery = this.escapeSolrQuery(query);
@@ -102,10 +150,16 @@ class SolrCollection {
         }
         catch (e) {
             console.error(e);
-            return Promise.resolve(this.emptyResponse);
+            return Promise.resolve([]);
         }
     }
     
+    /**
+     * Performs a search in the Solr core and returns the matching documents from the attached database.
+     * @param query Query to search for.
+     * @param options Options for the search (e.g. number of results to return or whether to include text fields)
+     * @returns Array of matching documents with their relevance scores.
+     */
     async search(query: string, options?: { rows?: number, includeTextFields?: boolean }): Promise<any[]> {
         const idsWithScores = await this.searchIds(query, options);
 
@@ -138,6 +192,12 @@ class SolrCollection {
             .sort((a: any, b: any) => b.score - a.score);
     }
 
+    /**
+     * Performs a suggest query in the Solr core and returns the suggestions.
+     * @param query Query to search for.
+     * @param options Options for the suggest query (e.g. number of results to return or the suggestion language)
+     * @returns Array of suggestions.
+     */
     async suggest(query: string, options?: { rows?: number, lang?: 'cs' | 'en' }): Promise<any[]> {
         const url = new URL(this.getSelfUrl('suggest'))
         url.searchParams.append('suggest.q', query);
@@ -150,6 +210,9 @@ class SolrCollection {
     }
 }
 
+/**
+ * Solr is a wrapper around a Solr instance with a simple API for creating and accessing Solr cores.
+ */
 export class Solr {
     public url: string;
     public database: typeof db;
@@ -158,6 +221,10 @@ export class Solr {
         this.database = database;
     }
 
+    /**
+     * @param collection The name of the Solr core.
+     * @returns The `SolrCollection` with the given name.
+     */
     getCollection(collection: string) {
         return new SolrCollection(this, collection);
     }
