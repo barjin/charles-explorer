31e828975b25edad324ce359c014b2fcf1219e9d
Jindřich Bär <jindrichbar@gmail.com>
2023-12-18 14:00:13 +0100

    fix: smaller legend

--- packages/webapp/app/components/WordCloud/Legends/FacultiesLegend.tsx
+++ packages/webapp/app/components/WordCloud/Legends/FacultiesLegend.tsx
@@ -3,14 +3,17 @@ import { getFacultyColor } from "~/utils/colors";
 import { localize } from "~/utils/lang";
 
 export function FacultiesLegend({ faculties }: { faculties: any[] }) {
-    const { i18n: { language: lang } } = useTranslation();
+    const { i18n: { language: lang }, t } = useTranslation();
 
     return faculties.length > 0 && (
-        <div className="flex flex-col justify-center bg-white rounded-md p-5 shadow-sm">
+        <div className="flex flex-col justify-center bg-white rounded-md p-2 shadow-sm">
+            <div className="text-sm font-semibold mb-1 text-center">
+                { t('legend') }
+            </div>
             {
                 faculties.map((faculty: any) => (
                     <div className="mt-1" key={faculty.id}>
-                        <div className="flex items-center">
+                        <div className="flex items-center text-sm">
                             <div 
                                 className="w-3 h-3 rounded-full mr-3"
                                 style={{ backgroundColor: getFacultyColor(faculty.id) }}    
