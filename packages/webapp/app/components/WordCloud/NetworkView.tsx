541158f4f19bf2f47d8eeff579f056cc05f0d9dd
Jindřich Bär <jindrichbar@gmail.com>
2024-03-17 09:59:20 +0100

    feat: initial connection to memgraph

--- packages/webapp/app/components/WordCloud/NetworkView.tsx
+++ packages/webapp/app/components/WordCloud/NetworkView.tsx
@@ -1,10 +1,22 @@
+import { useFetcher, useParams, useSearchParams } from "@remix-run/react";
+import { useEffect } from "react";
+
 /**
  * Renders a social network view on entities and their relationships
  */
 export function NetworkView() {
+    const fetcher = useFetcher();
+    const params = useParams();
+
+    useEffect(() => {
+        fetcher.load(`/person/network?id=${params.id}`);
+    }, []);
+    
     return (            
         <div className="w-full h-full text-white relative bg-slate-800">
-            this is a network view placeholder
+            this is a network view placeholder {
+                JSON.stringify(fetcher.data, null, 2)
+            }
         </div>
     );
 }
--- packages/webapp/app/routes/$category/network.tsx
+++ packages/webapp/app/routes/$category/network.tsx
@@ -1,44 +1,30 @@
 import { type LoaderArgs } from "@remix-run/node";
-import { db } from "~/connectors/prisma";
-import axios from 'axios';
-import { normalizeName } from "~/utils/people";
+import graph from 'neo4j-driver';
+import { db } from '~/connectors/prisma';
 
-const emptyResponse = {
-    entities: [],
-    relations: []
-};
 
-interface Node {
-    id: string;
-    alternativeIds?: string[],
-    type: string;
-    title: string;
-    faculty: {
-        id: string;
-        names: { value: string }[];
-    };
-}
-
-interface Edge {
-    source: string;
-    target: string;
-    score: number;
-}
-
-interface SocialNetwork {
-    entities: Node[];
-    relations: Edge[];
-}
-
+const driver = graph.driver('bolt://localhost:7687');
 /**
  * Given a set of ids, returns a set of nodes and edges that represent the social network of the given ids
  * A social network of ids are an induced subgraph of the entire social network graph.
  * @param param0 
  * @returns 
  */
-export async function loader({ request, params }: LoaderArgs): Promise<SocialNetwork> {
-    return {
-        entities: [],
-        relations: [],
-    }
+export async function loader({ request, params }: LoaderArgs): Promise<any> {
+    const graphSession = await driver.session({ defaultAccessMode: graph.session.READ });
+
+    const ids = new URL(request.url).searchParams.getAll('id');
+
+    const result = await graphSession.executeRead((tx) => 
+        tx.run(`
+            match 
+                p=(origin:person {PERSON_ID: $personId})-->(pub)<--(c)
+                with project(p) as ego_subgraph
+            return ego_subgraph;`, 
+            { personId: ids[0] }
+        )
+    );
+
+    await graphSession.close();
+    return result.records[0]._fields[0];
 }
