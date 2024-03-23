23b53c9c221816aeeef3bbfc26b0057e34769420
Jindřich Bär <jindrichbar@gmail.com>
2024-03-23 08:22:37 +0100

    feat: add memgraph backend endpoint

--- packages/webapp/app/routes/$category/network.tsx
+++ packages/webapp/app/routes/$category/network.tsx
@@ -1,5 +1,5 @@
 import { type LoaderArgs } from "@remix-run/node";
-import graph from 'neo4j-driver';
+import graph, { Integer, Node } from 'neo4j-driver';
 import { db } from '~/connectors/prisma';
 
 
@@ -11,20 +11,108 @@ const driver = graph.driver('bolt://localhost:7687');
  * @returns 
  */
 export async function loader({ request, params }: LoaderArgs): Promise<any> {
-    const graphSession = await driver.session({ defaultAccessMode: graph.session.READ });
-
     const ids = new URL(request.url).searchParams.getAll('id');
 
-    const result = await graphSession.executeRead((tx) => 
+    const retrievedGraphP = (async () => {
+        const session = await driver.session({ defaultAccessMode: graph.session.READ });
+        const result = await session.executeRead((tx) => 
+            tx.run(`
+                match 
+                    p=(origin:person {PERSON_ID: $personId})-->(pub)<--(c)
+                    with project(p) as ego_subgraph
+                return ego_subgraph;`, 
+                { personId: ids[0] }
+            )
+        );
+        await session.close();
+        return result;
+    })()
+    
+    const identitiesP = (async () => {
+        const session = await driver.session({ defaultAccessMode: graph.session.READ });
+        const result = await session.executeRead((tx) => 
         tx.run(`
             match 
-                p=(origin:person {PERSON_ID: $personId})-->(pub)<--(c)
-                with project(p) as ego_subgraph
-            return ego_subgraph;`, 
-            { personId: ids[0] }
-        )
-    );
+                (origin:person {PERSON_ID: $personId})-->(pub)<--(c)
+            with 
+                reduce(
+                accumulator = "", 
+                variable IN collections.sort(
+                    collections.remove_all(
+                    split(
+                        toLower(c.PERSON_NAME), 
+                        ' '), 
+                    [ "doc.", "ph.d.", "phd", "rndr.", "mgr.", "prof.", "csc.", "ing.", "m.sc.", "mudr.", "dipl.-inform.", "dipl.-ing.", "bc.", "drsc."]
+                    )
+                ) | accumulator + " " + variable
+                ) as name_normalized, 
+                MIN(c.PERSON_NAME) as name,
+                collect(DISTINCT c.PERSON_ID) as ids, 
+                collect(DISTINCT c) as nodes,
+                count(pub) as common_pub_count
+            return name_normalized, name, ids, common_pub_count, nodes
+            order by common_pub_count desc;`, 
+                { personId: ids[0] }
+            )
+        );
+        await session.close();
+        return result;
+    })();
+    
+    const communitiesP = (async () => {
+        const session = await driver.session({ defaultAccessMode: graph.session.READ });
+        const result = await session.executeRead((tx) => 
+        tx.run(`
+            match 
+            p=((:person {PERSON_ID: $personId})-[*2]-())
+            with project(p) as ego_subgraph
+                call community_detection.get_subgraph(ego_subgraph.nodes, ego_subgraph.edges)
+                yield node, community_id
+            return node, community_id as community
+            order by community_id;`, { personId: ids[0] }
+            )
+        );
+        await session.close();
+        return result;
+    })();
 
-    await graphSession.close();
-    return result.records[0]._fields[0];
+    const [retrievedGraph, identities, communities] = await Promise.all([retrievedGraphP, identitiesP, communitiesP]);
+
+    const communityMap = new Map();
+
+    const communityArray = communities.records.map(x => ({node: (x.get('node') as Node).elementId, community: (x.get('community') as Integer).toNumber()}));
+
+    for (const { node, community } of communityArray) {
+        communityMap.set(node, community);
+    }
+
+    const nodeIdentityMap = new Map<string, string>();
+    const outGraph = {
+        nodes: retrievedGraph.records[0]._fields[0].nodes.map((node: Node) => {
+            const identity = identities.records.find(r => r.get('nodes').some((x: Node) => x.identity.toNumber() === node.identity.toNumber()));
+
+            if(nodeIdentityMap.has(node.elementId)) return false;
+    
+            identity?.get("nodes")?.forEach((alternative: Node) => {
+                nodeIdentityMap.set(alternative.elementId, node.elementId);
+            });
+
+            nodeIdentityMap.set(node.elementId, node.elementId);
+    
+            return {
+                ...node.properties,
+                id: node.elementId,
+                label: node.labels[0],
+                alternativeIds: identity?.get("ids"),
+                community: communityMap.get(node.elementId),
+            }
+        }).filter(x => x),
+        edges: retrievedGraph.records[0]._fields[0].edges.map(edge => ({
+            type: edge.type,
+            source: nodeIdentityMap.get(edge.startNodeElementId),
+            target: edge.endNodeElementId,
+        })),
+    };
+
+    return outGraph;
 }
